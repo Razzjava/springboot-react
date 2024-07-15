093a555db261d943859734a28e0d386eccf106df
Raz <33026305+Razzjava@users.noreply.github.com>
2024-07-15 17:23:06 +0100

    add fetch to post review to server

--- frontend/movies-frontend/src/components/PostReview.jsx
+++ frontend/movies-frontend/src/components/PostReview.jsx
@@ -1,10 +1,22 @@
 import { useState } from "react";
 
-function PostReview(){
+function PostReview({id}){
      const [review, setReview] = useState("");
+     const [name, setName] = useState("")
 
     const handleReview = (e) =>{
         setReview(e.target.value)
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        const newReview = {"review": review, "movieId": id, "name": name}
+        console.log(newReview)
+        fetch("http://localhost:9001/api/v1/createreview", {
+            method: 'POST',
+            headers:{"Content-Type": "application/json"},
+            body:JSON.stringify(newReview)
+        })
     }
 
     return (
@@ -14,7 +26,8 @@ function PostReview(){
             <label htmlFor="reviewInput">Add Review</label>
             <br></br>
             <textarea id="reviewInput" onChange={handleReview}></textarea>
-            <button className={"btn btn-primary"}>Post Review</button>
+            <br></br>
+            <button className={"btn btn-primary"} onClick={handleSubmit}>Post Review</button>
         </>
     )
 }
--- frontend/movies-frontend/src/components/ViewMovie.jsx
+++ frontend/movies-frontend/src/components/ViewMovie.jsx
@@ -7,22 +7,24 @@ function ViewMovie({id, selectedMovie}){
 
     return(
         <>
-            <div className="modal fade" id="viewMovie" tabIndex="-1" aria-labelledby="viewMovie"
+            <div className="modal fade " id="viewMovie" tabIndex="-1" aria-labelledby="viewMovie"
                  aria-hidden="true">
-                <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-                    <div className="modal-content">
+                <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable min-vw-100">
+                    <div className="modal-content min-vh-100 ">
                         <div className="modal-header">
                             <h1 className="modal-title fs-5" id="exampleModalLabel">{selectedMovie ==null? "": selectedMovie.name}</h1>
                             <button type="button" className="btn-close" data-bs-dismiss="modal"
                                     aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
+                            <h4>Description</h4>
+                            <br></br>
                             {selectedMovie ==null? "": selectedMovie.description}
                             <br></br>
 
                         </div>
-                        <div className={"modal-footer"}>
-                            <PostReview></PostReview>
+                        <div className={"mx-auto p-2 borderReview"}>
+                            <PostReview id={id}></PostReview>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
